c3c0f821e8f3f5b871450b8347fdbad5c49e56cb
Anibal Ventura <anibalventura20@gmail.com>
2021-12-29 15:27:41 -0400

    Add screens UI.

---
+++ SplitCheck/ViewControllers/CalculateViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  SplitCheck
+//
+//  Created by Anibal Ventura on 29/12/21.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+}
---
+++ SplitCheck/ViewControllers/ResultViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ResultViewController.swift
+//  SplitCheck
+//
+//  Created by Anibal Ventura on 29/12/21.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+}
